93164417b135c8ded3ff7c9be755f3ff5a9aea51
Daphne Carvalho <dah.feh@hotmail.com>
2022-02-01 02:51:38 -0300

    perf: apply performance improvements (memo, useCallback and virtualization)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,11 @@
+import { AutoSizer, List, ListRowRenderer } from "react-virtualized";
+
 import { Button } from "./Button";
 
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
@@ -13,24 +15,44 @@ interface SideBarProps {
 export function SideBar({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  buttonClickCallback,
 }: SideBarProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    const genre = genres[index];
+    return (
+      <div key={key} style={style}>
+        <Button
+          key={genre.id}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => buttonClickCallback(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      </div>
+    );
+  };
+
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
-            selected={selectedGenreId === genre.id}
-          />
-        ))}
+        <AutoSizer disableHeight>
+          {({ width }) => {
+            return (
+              <List
+                height={document.body.offsetHeight - 200}
+                rowHeight={75}
+                rowCount={genres.length}
+                rowRenderer={rowRenderer}
+                width={width}
+              />
+            );
+          }}
+        </AutoSizer>
       </div>
-
     </nav>
-  )
+  );
 }
